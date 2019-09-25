5dcfec92ff1ec8e5cf19909616edc661639e0af7
Xin Yao <xinyao@Xins-MacBook-Pro-2.local>
2019-09-24 22:59:05 -0700

    Added coffeeScript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
 var gulp = require("gulp");
 var gUtil = require("gulp-util");
+var gCoffee = require("gulp-coffee");
+
+var coffeeSources = ["components/coffee/*.coffee"];
 
 gulp.task("log", function() {
     gUtil.log("Workflows are awesome!");
+}); // create a task called log
+
+gulp.task("coffee", function() {
+    gulp.src(coffeeSources)// define where is the original source location
+        .pipe(gCoffee({ bare: true })
+            .on("error", gUtil.log))// once we get an error, we should catch it
+        .pipe(gulp.dest("components/scripts"))// where we are going to send the file to once this process is done
 }); // create a task called log
