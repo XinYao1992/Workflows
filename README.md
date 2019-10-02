03b70271784197e0ebbc9dd439a66a3ff34a8216
Xin Yao <xinyao@Xins-MacBook-Pro-2.local>
2019-10-02 15:35:29 -0700

    Added jquery, mustache and browserify

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Workflows Course
 
 This project is an example of workflow management for a lynda.com course.
+
+Technology stack: 
+jQuery: to take care of JSON importing
+Mustache.js: to take care of JavaScript templating
+browserify: adding plugins and dependencies
--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require("gulp");
 var gUtil = require("gulp-util");
 var gCoffee = require("gulp-coffee");
 var gConcat = require("gulp-concat");
+var browserify = require("gulp-browserify");
 
 var coffeeSources = ["components/coffee/*.coffee"];
 var jsSources = [
@@ -11,19 +12,20 @@ var jsSources = [
     "components/scripts/tagline.js"
 ];
 
-gulp.task("log", function() {
+gulp.task("log", async function() {
     gUtil.log("Workflows are awesome!");
 }); // create a task called log
 
-gulp.task("coffee", function() {
+gulp.task("coffee", async function() {
     gulp.src(coffeeSources)// define where is the original source location
         .pipe(gCoffee({ bare: true })
             .on("error", gUtil.log))// once we get an error, we should catch it
         .pipe(gulp.dest("components/scripts"))// where we are going to send the file to once this process is done
 }); // create a task called log
 
-gulp.task("jsCombineAll", function() {
+gulp.task("jsCombineAll", async function() {
     gulp.src(jsSources)
         .pipe(gConcat("script.js"))
+        .pipe(browserify())// adding all plugins and dependencies, such as jquery and mustache, into application
         .pipe(gulp.dest("builds/development/js"))
 });
